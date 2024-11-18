d19cebb0329d00e46fe9cd8b5a720513617afc2f
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2024-11-18 15:03:30 +0000

    file: add helper func to extract urls from html

---
+++ src/utils/extractURLs.js
@@ -0,0 +1,25 @@
+const { JSDOM } = require("jsdom");
+
+function extractURLs(htmlString, baseUrl) {
+  const urls = [];
+  const dom = new JSDOM(htmlString);
+  const linkElements = [...dom.window.document.querySelectorAll("a")];
+
+  linkElements.forEach((linkElement) => {
+    try {
+      let urlObj;
+      if (linkElement.href.charAt(0) === "/") {
+        urlObj = new URL(`${baseUrl}${linkElement.href}`);
+      } else {
+        urlObj = new URL(linkElement.href);
+      }
+      urls.push(urlObj.href);
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+  return urls;
+}
+
+module.exports = extractURLs;
